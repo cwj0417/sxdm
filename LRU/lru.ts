d8c26ee8ef2bdbe9926aa2b9fc934fb000e71a0a
chenwj <731028571@qq.com>
2021-04-01 18:09:56 +0800

    feat: 增加LRU

---
+++ LRU/lru.ts
@@ -0,0 +1,28 @@
+export class LRUCache<T = any, K extends number | string = number> {
+    private cache: {
+        [key: string]: T
+    } = {}
+    private keys: K[] = []
+
+    constructor(private capacity: number) {}
+
+    get (key: K) {
+        if (this.keys.indexOf(key) > -1) {
+            this.keys.splice(this.keys.indexOf(key), 1)
+            this.keys.push(key)
+            return this.cache[key]
+        }
+        return false
+    }
+    put (key: K, value: T) {
+        if (this.keys.indexOf(key) > -1) {
+            this.cache[key] = value;
+        } else {
+            if (this.keys.length >= this.capacity) {
+                delete this.cache[this.keys.shift()]
+            }
+            this.cache[key] = value
+            this.keys.push(key)
+        }
+    }
+}
---
+++ LRU/test.ts
@@ -0,0 +1,13 @@
+import { LRUCache } from './lru';
+
+test('lru', () => {
+    const cache = new LRUCache<number, number>(3);
+    cache.put(1, 2);
+    cache.put(1, 2);
+    cache.put(1, 2);
+    
+    expect(cache.get(1)).toBe(2);
+    cache.put(4, 2);
+    expect(cache.get(2)).toBe(false);
+    console.log(cache)
+})
